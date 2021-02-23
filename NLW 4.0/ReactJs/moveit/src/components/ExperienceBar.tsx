ed1837f86d1d3b61d91c89b747fa12440bfc1226
Anderson Pereira Rodrigues <anderson.carro.95@gmail.com>
2021-02-23 02:21:24 -0300

    Inicio Semana NLW 4.0 Trilhas ReactJs e NodeJs

---
+++ NLW 4.0/ReactJs/moveit/src/components/ExperienceBar.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../styles/global.css";
+export function ExperienceBar() {
+  return (
+    <header className="experience-bar">
+      <span>8 xp</span>
+      <div>
+        <div style={{ width: "50%" }}>
+          <span className="current-experience" style={{ left: "50%" }}>
+            300 xp
+          </span>
+        </div>
+      </div>
+      <span>600 xp</span>
+    </header>
+  );
+}
